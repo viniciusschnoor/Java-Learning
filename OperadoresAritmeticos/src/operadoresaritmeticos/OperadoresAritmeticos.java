fb2591b835b0de9ea433a7ce76959d4e0c35a3be
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-17 21:27:56 -0300

    Operadores

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,42 @@
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author vschn
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2;
+        System.out.printf("A média é %.2f", m);
+        */
+        
+        /*
+        int numero = 5;
+        int valor = 5 + ++numero;
+        System.out.println(valor);
+        */
+        
+        /*
+        int x = 4;
+        x *= 2; // x = x + 2
+        System.out.println(x);
+        */
+        
+        /*
+        float v = 8.5f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);
+        */
+        
+        double ale = Math.random();
+        System.out.println(Math.round(ale * 100));
+    }
+    
+}
